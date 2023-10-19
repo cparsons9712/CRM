bf04b176dd03651e5c8a73a2fa54549316551366
christine parsons <clong9712@gmail.com>
2023-10-19 19:47:08 -0400

    scroll bar buttonsgit add .git add .git add .

--- react-app/src/components/Task/TaskComponent.js
+++ react-app/src/components/Task/TaskComponent.js
@@ -2,7 +2,7 @@
 import "./task.css"
 import TaskDetails from "./details";
 import { useModal } from "../../context/Modal";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import { updateTask, completeTask} from "../../store/task";
 import { useDispatch, useSelector } from "react-redux";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -12,6 +12,7 @@ import { convertDate } from "../../util";
 
 function TaskComponent ({task}){
     const dispatch = useDispatch()
+    const contentRef = useRef(null);
 
     const { setModalContent } = useModal();
 
@@ -20,6 +21,20 @@ function TaskComponent ({task}){
         setModalContent(<TaskDetails task={t} />);
     }
     task.sort((a, b) => new Date(a.due_date) - new Date(b.due_date));
+
+    const scrollLeft = () => {
+        const content = contentRef.current;
+        if (content) {
+          content.scrollLeft -= 150; // Adjust the scroll distance as needed
+        }
+      };
+
+      const scrollRight = () => {
+        const content = contentRef.current;
+        if (content) {
+          content.scrollLeft += 150; // Adjust the scroll distance as needed
+        }
+      };
 
 
     const getColor = (priority) => {
@@ -40,45 +55,47 @@ function TaskComponent ({task}){
 
         return (
             <div className="sliceCont">
+                <button className="scroll-button left" onClick={scrollLeft}>&lt;</button>
+                <div className="scrollable-content" ref={contentRef}>
+                        {task.map((t)=> {
 
-                {task.map((t)=> {
-
-                    if(t){
-                    return (
-                        <div className="taskTile" key={t.id} onClick={()=>{handleTaskClick(t)}}>
-                            <div
-                            className="taskHeader"
-                            style={{backgroundColor: getColor(t.priority), color: 'white'}}
-                            >
-
-
-                                <div
-                                className="taskDueDate">
-                                    {t?.due_date ? convertDate(t.due_date) : "none"}
+                            if(t){
+                            return (
+                                <div className="taskTile" key={t.id} onClick={()=>{handleTaskClick(t)}}>
+                                    <div
+                                    className="taskHeader"
+                                    style={{backgroundColor: getColor(t.priority), color: 'white'}}
+                                    >
 
 
+                                        <div
+                                        className="taskDueDate">
+                                            {t?.due_date ? convertDate(t.due_date) : "none"}
+
+
+                                        </div>
+
+
+                                    </div>
+                                <div className="TaskBody">
+                                        <p className="taskDescription">
+                                            {t.description}
+                                        </p>
+                                        <div className="taskClient" >
+                                            {t.Client? `${t.Client.firstName} ${t.Client.lastName.slice(0,1)}.`: ''}
+                                        </div>
                                 </div>
 
 
                             </div>
-                        <div className="TaskBody">
-                                <p className="taskDescription">
-                                    {t.description}
-                                </p>
-                                <div className="taskClient" >
-                                    {t.Client? `${t.Client.firstName} ${t.Client.lastName.slice(0,1)}.`: ''}
-                                </div>
-                        </div>
 
-
-                    </div>
-                )}else {
-                    return null
-                }
-
-            })}
-
-        </div>
+                        )}else {
+                            return null
+                        }
+                    })}
+                </div>
+                <button class="scroll-button right" onClick={scrollRight}>&gt;</button>
+            </div>
     )}else{return <div className="sliceCont"></div>}
 }
 export default TaskComponent
